0745bda84363310c2be822f01452c7d65735b825
atomicduck <ngtrinhthuc@gmail.com>
2024-05-18 17:13:40 +0700

    push

--- report-service/README.md
+++
@@ -1,5 +0,0 @@
-### Kafka NestJS and KafkaJS
-
-#### video tutorial can find in bellow link
-
-https://www.youtube.com/watch?v=OjpS2u3gclI
--- report-service/src/roadstatus/dto/update-roadstatus.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRoadstatusDto } from './create-roadstatus.dto';
-
-export class UpdateRoadstatusDto extends PartialType(CreateRoadstatusDto) {}
